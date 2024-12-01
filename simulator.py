9f2b0def3ae04e4effaac14f021bb08049497932
Takuya Saito <takus4649@gmail.com>
2024-12-02 05:25:17 +0900

    スコア計算を実装

--- simulator.py
+++ simulator.py
@@ -25,7 +25,8 @@ def main(i):
     S = r['S']
     estimated_score = r['estimated score']
     score = r['score']
-    data = [i, N, T, S, estimated_score, score, t]
+    real_score = r['real score']
+    data = [i, N, T, S, estimated_score, score, real_score, t]
     print('\r', 'end', i, end='')
     # print(i, 'end')
     return data
@@ -47,12 +48,15 @@ if __name__ == '__main__':
         result = [d.get() for d in data]
     print()
     # '''
-    df = pd.DataFrame(result, columns=['i', 'N', 'T', 'S', 'estimated score', 'score', 'time'])
+    df = pd.DataFrame(result, columns=['i', 'N', 'T', 'S', 'estimated score', 'score', 'real score', 'time'])
     estimated_score = np.mean(df['estimated score'])
     score = np.mean(df['score'])
+    real_score = np.mean(df['real score'])
     sum_score = score * 50
     sum_estimated_score = estimated_score * 50
-    print(f"score: {format(int(sum_score), ',')}, score mean: {format(int(score), ',')}")
+    sum_real_score = real_score * 50
+    print(f"judge score: {format(int(sum_score), ',')}, score mean: {format(int(score), ',')}")
     print(f"estimated_score: {format(int(sum_estimated_score), ',')}, score mean: {format(int(estimated_score), ',')}")
+    print(f"real_score: {format(int(sum_real_score), ',')}, score mean: {format(int(real_score), ',')}")
     df.to_csv('result.csv', index=False)
     print(f'end elapsed time: {time.time()-start:.2f}s')
--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,25 @@ impl Solver {
         Solver { n, t, s, wh, judge }
     }
 
-    fn ans(&mut self, r#box: Box) -> (usize, usize) {
+    fn ans(&mut self, r#box: Box) -> (usize, usize, usize) {
         let estimated_score = r#box.get_score();
+        let real_score = self.judge.real_box.get_score();
+        self.judge.comment(format!("turn: {}", self.judge.turn+1));
+        self.judge.comment(format!("estimated (w, h, score): ({}, {}, {})", r#box.max_x, r#box.max_y, estimated_score));
+        self.judge.comment(format!("real (w, h, score): ({}, {}, {})", self.judge.real_box.max_x, self.judge.real_box.max_y, real_score));
         let (w, h) = self.judge.ans(r#box);
-        self.judge.comment(format!("turn: {}", self.judge.turn));
-        self.judge.comment(format!("estimated score: {}", estimated_score));
         let score = w + h;
-        self.judge.comment(format!("result score: {}", score));
 
-        (estimated_score, score)
+        (estimated_score, score, real_score)
+    }
+
+    fn add(&mut self, r#box: &mut Box, i: usize, r: usize, d: char, b: isize) {
+        r#box.add(self.wh[i], r, d, b);
+
+        // ローカル実行の場合は本当のサイズのBoxを算出
+        if cfg!(debug_assertions) {
+            self.judge.real_box.add(self.judge.wh[i], r, d, b);
+        }
     }
 
     fn optimize(&mut self) -> Box {
@@ -37,8 +47,9 @@ impl Solver {
         // グッズを順に追加する
         for i in 0..self.n {
             // 一つ前の下に合わせてref_hを超えたら次の行に行く
-            r#box.add(self.wh[i], 0, 'L', pre_b);
-            sum_h += self.wh[i].1;
+            let (r, d) = r#box.opt_instruction(self.wh[i], 'L', pre_b);
+            self.add(&mut r#box, i, r, d, pre_b);
+            sum_h += if r == 0 { self.wh[i].1 } else { self.wh[i].0 };
             pre_b = i as isize;
             if sum_h > ref_h {
                 sum_h = 0;
@@ -52,18 +63,20 @@ impl Solver {
     fn solve(&mut self) {
         let mut opt_estimated_score = usize::MAX;
         let mut opt_score = usize::MAX;
+        let mut opt_real_score = usize::MAX;
         for _ in 0..self.t {
             let r#box = self.optimize();
 
-            let (estimated_score, score) = self.ans(r#box);
+            let (estimated_score, score, real_score) = self.ans(r#box);
 
             opt_estimated_score = opt_estimated_score.min(estimated_score);
             opt_score = opt_score.min(score);
+            opt_real_score = opt_real_score.min(real_score);
         }
 
         // 結果出力
-        self.judge.comment(format!("estimated score: {}, score: {}", opt_estimated_score, opt_score));
-        eprintln!("{{ \"N\": {}, \"T\": {}, \"S\": {}, \"estimated score\": {}, \"score\": {} }}", self.n, self.t, self.s, opt_estimated_score, opt_score);
+        self.judge.comment(format!("estimated score: {}, score: {}, real_score: {}", opt_estimated_score, opt_score, opt_real_score));
+        eprintln!("{{ \"N\": {}, \"T\": {}, \"S\": {}, \"estimated score\": {}, \"score\": {}, \"real score\": {} }}", self.n, self.t, self.s, opt_estimated_score, opt_score, opt_real_score);
     }
 }
 
@@ -74,11 +87,13 @@ struct Judge {
     wh: Vec<(usize, usize)>,
     dwh: Vec<(isize, isize)>,
     turn: usize,
+    real_box: Box,
 }
 
 impl Judge {
     fn new(n: usize, t: usize, s: usize) -> Judge {
         let turn = 0;
+        let real_box = Box::new();
         
         // ローカル実行時
         let (wh, dwh) = if cfg!(debug_assertions) {
@@ -91,14 +106,14 @@ impl Judge {
             (Vec::new(), Vec::new())
         };
 
-        Judge { n, t, s, wh, dwh, turn }
+        Judge { n, t, s, wh, dwh, turn, real_box }
     }
 
     fn ans(&mut self, r#box: Box) -> (usize, usize) {
         r#box.ans();
         
         let (w, h) = if cfg!(debug_assertions) {
-            let (w, h) = (r#box.max_x, r#box.max_y);  // TODO: 実際のwhでスコアを再計算
+            let (w, h) = (self.real_box.max_x, self.real_box.max_y);
             let (dw, dh) = self.dwh[self.turn];
             let (w, h) = (w as isize +dw, h as isize +dh);
             let w = if w > 0 { w as usize } else { 1 };
@@ -113,6 +128,11 @@ impl Judge {
         };
 
         self.turn += 1;
+        self.comment(format!("turn: {}", self.turn));
+        self.comment(format!("estimated (w, h, score): ({}, {}, {})", r#box.max_x, r#box.max_y, r#box.get_score()));
+        self.comment(format!("judge (w, h, score): ({}, {}, {})", w, h, w+h));
+        self.comment(format!("real (w, h, score): ({}, {}, {})", self.real_box.max_x, self.real_box.max_y, self.real_box.get_score()));
+        self.real_box = Box::new();
         (w, h)
     }
 
@@ -126,6 +146,7 @@ struct Box {
     status: Vec<(usize, usize, usize, usize)>,
     max_x: usize,
     max_y: usize,
+    sum_area: usize,
     order: Vec<(usize, char, isize)>,
 }
 
@@ -135,14 +156,42 @@ impl Box {
         let status = Vec::new();
         let max_x = 0;
         let max_y = 0;
+        let sum_area = 0;
         let order = Vec::new();
-        Box { goods, status, max_x, max_y, order }
+        Box { goods, status, max_x, max_y, sum_area, order }
+    }
+
+    fn opt_instruction(&self, wh: (usize, usize), d: char, b: isize) -> (usize, char) {
+        let mut opt_r = 0;
+        let mut opt_eval = 0.0;
+
+        for r in [0, 1] {
+            let (x1, y1, x2, y2) = self.put(wh, r, d, b);
+            let max_x = self.max_x.max(x2);
+            let max_y = self.max_y.max(y2);
+            let eval = (self.sum_area + (wh.0 * wh.1)) as f64 / (max_x * max_y) as f64;
+            if eval > opt_eval {
+                opt_eval = eval;
+                opt_r = r;
+            }
+        }
+
+        (opt_r, d)
     }
 
     fn add(&mut self, wh: (usize, usize), r: usize, d: char, b: isize) {
         self.goods.push(wh);
         self.order.push((r, d, b));
 
+        let (x1, y1, x2, y2) = self.put(wh, r, d, b);
+
+        self.status.push((x1, y1, x2, y2));
+        self.max_x = self.max_x.max(x2);
+        self.max_y = self.max_y.max(y2);
+        self.sum_area += wh.0 * wh.1;
+    }
+
+    fn put(&self, wh: (usize, usize), r: usize, d: char, b: isize) -> (usize, usize, usize, usize) {
         let (dx, dy) = if r == 0 { wh } else { (wh.1, wh.0) };
         let (x1, y1, x2, y2) = if d == 'U' {
             // d == 'U'の場合
@@ -160,14 +209,12 @@ impl Box {
             (x1, y1, x2, y2)
         };
 
-        self.status.push((x1, y1, x2, y2));
-        self.max_x = self.max_x.max(x2);
-        self.max_y = self.max_y.max(y2);
+        (x1, y1, x2, y2)
     }
 
     fn max_x(&self, in_y1: usize, in_y2: usize) -> usize {
         let mut max_x = 0;
-        for &(_, x2, y1, y2) in self.status.iter() {
+        for &(_, y1, x2, y2) in self.status.iter() {
             if in_y1 < y2 && y1 < in_y2 {
                 max_x = max_x.max(x2);
             }
