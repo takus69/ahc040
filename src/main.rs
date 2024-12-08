f54beec7130044beaf19f0c2c52f664d2d1c26dc
Takuya Saito <takus4649@gmail.com>
2024-12-08 21:29:52 +0900

    v3: ビームサーチ実装(予測: 56,131,606, 提出: 60,013,729)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use proconio::{input, input_interactive};
+use proconio::input_interactive;
+use std::collections::{VecDeque, BinaryHeap};
+use rand::Rng;
+use rand::SeedableRng;
+use rand::rngs::StdRng;
 
 struct Solver {
     n: usize,
@@ -26,53 +30,113 @@ impl Solver {
         (estimated_score, score, real_score)
     }
 
-    fn add(&mut self, r#box: &mut Box, i: usize, r: usize, d: char, b: isize) {
-        r#box.add(self.wh[i], r, d, b);
+    fn add(&mut self, r#box: &mut Box, i: usize, r: usize, d: char, col_i: usize) {
+        r#box.add(self.wh[i], r, d, col_i);
 
         // ローカル実行の場合は本当のサイズのBoxを算出
         if cfg!(debug_assertions) {
-            self.judge.real_box.add(self.judge.wh[i], r, d, b);
+            self.judge.real_box.add(self.judge.wh[i], r, d, col_i);
         }
     }
 
-    fn optimize(&mut self) -> Box {
-        let mut r#box = Box::new();
+    fn playout(&mut self, org_box: &Box, next_i: usize) -> usize {
+        let mut r#box = org_box.clone();
+        for i in next_i..self.n {
+            let col_i = r#box.start_col_i;
+            let (r, d) = r#box.opt_instruction(self.wh[i], 'L', col_i);
+            self.add(&mut r#box, i, r, d, col_i);
+        }
+
+        r#box.get_score()
+    }
+
+    fn optimize(&mut self) -> BinaryHeap<(usize, Box)> {
+        // 乱数
+        let seed: u64 = 42;
+        let mut rng = StdRng::seed_from_u64(seed);
+
+        // 最大のhを決める
         let mut sum_area = 0;
         for (wi, hi) in self.wh.iter() {
             sum_area += wi * hi;
         }
         let ref_h = (sum_area as f64).sqrt() as usize;
-        let mut pre_b = -1;
-        let mut sum_h = 0;
+
+        // ビーム
+        let beams_width = 150 / self.n;
+        let trial = 150 / self.n;
+        let mut beams: BinaryHeap<(usize, Box)> = BinaryHeap::new();
+        let r#box = Box::new(ref_h);
+        beams.push((usize::MAX, r#box));
+
         // グッズを順に追加する
         for i in 0..self.n {
-            // 一つ前の下に合わせてref_hを超えたら次の行に行く
-            let (r, d) = r#box.opt_instruction(self.wh[i], 'L', pre_b);
-            self.add(&mut r#box, i, r, d, pre_b);
-            sum_h += if r == 0 { self.wh[i].1 } else { self.wh[i].0 };
-            pre_b = i as isize;
-            if sum_h > ref_h {
-                sum_h = 0;
-                pre_b = -1;
+            self.judge.comment(format!("i: {}", i));
+            let mut next_beams: BinaryHeap<(usize, Box)> = BinaryHeap::new();
+            while !beams.is_empty() {
+                let (_, mut r#box) = beams.pop().unwrap();
+                let org_box = r#box.clone();
+                for j in 0..trial {
+                    // パラメータをランダム選択
+                    let mut r = rng.gen_range(0..=1);
+                    let mut d = 'L';
+                    let mut col_i = rng.gen_range(r#box.start_col_i..=r#box.col_info.len());
+                    if j == 0 {
+                        (r, d) = r#box.opt_instruction(self.wh[i], 'L', r#box.start_col_i);
+                        col_i = r#box.start_col_i;
+                    }
+                    // TODO: ハッシュ関数で同一状態を確認
+                    self.add(&mut r#box, i, r, d, col_i);  // グッズを配置
+
+                    // プレイアウト
+                    let score = self.playout(&r#box, i+1);
+                    self.judge.comment(format!("trial: {}, playout score: {}", j, score));
+                    next_beams.push((score, r#box));
+                    while next_beams.len() > beams_width {
+                        next_beams.pop();
+                    }
+                    r#box = org_box.clone();
+                }
             }
+            beams = next_beams;
         }
 
-        r#box
+        beams
     }
 
     fn solve(&mut self) {
         let mut opt_estimated_score = usize::MAX;
         let mut opt_score = usize::MAX;
         let mut opt_real_score = usize::MAX;
-        for _ in 0..self.t {
-            let r#box = self.optimize();
+        let mut opt_box = Box::new(0);
 
-            let (estimated_score, score, real_score) = self.ans(r#box);
+        let mut beams = self.optimize();
+        // Vecに格納しなおして、スコアの小さい方から順に回答する
+        let mut target: VecDeque<Box> = VecDeque::new();
+        while !beams.is_empty() {
+            let (_, r#box) = beams.pop().unwrap();
+            target.push_back(r#box);
+        }
 
+        for _ in 0..(self.t-1) {
+            if target.is_empty() {
+                let r#box = Box::new(0);
+                self.ans(r#box);
+                continue;
+            }
+            let r#box = target.pop_back().unwrap();
+
+            let (estimated_score, score, real_score) = self.ans(r#box.clone());
+
+            if opt_estimated_score > estimated_score {
+                opt_box = r#box.clone();
+            }
             opt_estimated_score = opt_estimated_score.min(estimated_score);
             opt_score = opt_score.min(score);
             opt_real_score = opt_real_score.min(real_score);
         }
+        // 一番良さそうな結果を最後に提出
+        self.ans(opt_box);
 
         // 結果出力
         self.judge.comment(format!("estimated score: {}, score: {}, real_score: {}", opt_estimated_score, opt_score, opt_real_score));
@@ -93,7 +157,7 @@ struct Judge {
 impl Judge {
     fn new(n: usize, t: usize, s: usize) -> Judge {
         let turn = 0;
-        let real_box = Box::new();
+        let real_box = Box::new(0);
         
         // ローカル実行時
         let (wh, dwh) = if cfg!(debug_assertions) {
@@ -132,7 +196,7 @@ impl Judge {
         self.comment(format!("estimated (w, h, score): ({}, {}, {})", r#box.max_x, r#box.max_y, r#box.get_score()));
         self.comment(format!("judge (w, h, score): ({}, {}, {})", w, h, w+h));
         self.comment(format!("real (w, h, score): ({}, {}, {})", self.real_box.max_x, self.real_box.max_y, self.real_box.get_score()));
-        self.real_box = Box::new();
+        self.real_box = Box::new(self.real_box.ref_h);
         (w, h)
     }
 
@@ -141,43 +205,61 @@ impl Judge {
     }
 }
 
+#[derive(Debug, Eq, PartialEq, Clone)]
 struct Box {
     goods: Vec<(usize, usize)>,
     status: Vec<(usize, usize, usize, usize)>,
     max_x: usize,
     max_y: usize,
-    col_info: Vec<(usize, usize)>,  // (max_w, sum_h)
+    col_info: Vec<(usize, usize, usize)>,  // (max_w, sum_h, last_i)
     area_info: Vec<usize>,
     order: Vec<(usize, char, isize)>,
+    start_col_i: usize,
+    ref_h: usize,
+}
+
+impl Ord for Box {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (self.max_x + self.max_y).cmp(&(other.max_x + other.max_y))
+    }
+}
+
+impl PartialOrd for Box {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some((self.max_x + self.max_y).cmp(&(other.max_x + other.max_y)))
+    }
 }
 
 impl Box {
-    fn new() -> Box {
+    fn new(ref_h: usize) -> Box {
         let goods = Vec::new();
         let status = Vec::new();
         let max_x = 0;
         let max_y = 0;
-        let col_info = Vec::new();  // 列に追加したグッズの(max_w, sum_h)
+        let col_info = Vec::new();  // 列に追加したグッズの(max_w, sum_h, last_i)
         let area_info = Vec::new();  // 列に追加したグッズの面積の合計
         let order = Vec::new();
-        Box { goods, status, max_x, max_y, col_info, area_info, order }
+        let start_col_i = 0;
+        Box { goods, status, max_x, max_y, col_info, area_info, order, start_col_i, ref_h }
     }
 
-    fn opt_instruction(&self, wh: (usize, usize), d: char, b: isize) -> (usize, char) {
+    fn opt_instruction(&self, wh: (usize, usize), d: char, col_i: usize) -> (usize, char) {
         let mut opt_r = 0;
         let mut opt_eval = 0.0;
+        let i = self.goods.len();
+        let b = if col_i == self.col_info.len() { -1 } else { self.col_info[col_i].2 as isize };
 
         // 回転の評価
         for r in [0, 1] {
             // 一つ前の位置を取得
             let (x1, y1, x2, y2) = self.put(wh, r, d, b);
-            let (mut max_x, mut sum_y) = match self.col_info.last() {
+            let (mut max_x, mut sum_y, mut last_i) = match self.col_info.last() {
                 Some(&last) => { last },
-                None => { (0, 0) },
+                None => { (0, 0, i) },
             };
             max_x = max_x.max(x2);
-            sum_y = sum_y + (y2-y1);
-            let (pre_max_x, _) = if self.col_info.len() > 1 { self.col_info[self.col_info.len()-1] } else { (0, 0) };
+            sum_y += y2-y1;
+            let (pre_max_x, _, _) = if self.col_info.len() > 1 { self.col_info[self.col_info.len()-1] } else { (0, 0, i) };
             let area = match self.area_info.last() {
                 Some(&last) => last,
                 None => 0,
@@ -192,9 +274,11 @@ impl Box {
         (opt_r, d)
     }
 
-    fn add(&mut self, wh: (usize, usize), r: usize, d: char, b: isize) {
+    fn add(&mut self, wh: (usize, usize), r: usize, d: char, col_i: usize) {
+        let i = self.goods.len();
+        let b = if col_i == self.col_info.len() { -1 } else { self.col_info[col_i].2 as isize };
         if b == -1 {
-            self.col_info.push((0, 0));
+            self.col_info.push((0, 0, i));
             self.area_info.push(0);
         }
 
@@ -206,10 +290,14 @@ impl Box {
         self.status.push((x1, y1, x2, y2));
         self.max_x = self.max_x.max(x2);
         self.max_y = self.max_y.max(y2);
-        let (max_x, sum_y) = self.col_info.last_mut().unwrap();
-        *max_x = (*max_x).max(x2);
-        *sum_y += y2-y1;
-        *(self.area_info.last_mut().unwrap()) += wh.0 * wh.1;
+        let (mut max_x, mut sum_y, _) = self.col_info[col_i];
+        max_x = max_x.max(x2);
+        sum_y += y2-y1;
+        self.col_info[col_i] = (max_x, sum_y, i);
+        self.area_info[col_i] += wh.0 * wh.1;
+        if sum_y > self.ref_h {
+            self.start_col_i = col_i + 1;
+        }
     }
 
     fn put(&self, wh: (usize, usize), r: usize, d: char, b: isize) -> (usize, usize, usize, usize) {
